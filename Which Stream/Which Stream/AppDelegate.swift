075da9377239a54a8654ab8c5a5b35a43e506153
Leo Oliveira <macewandu@hotmail.com>
2018-08-12 00:38:06 -0400

    Fully functional authentication with Simkl. No other major functionality implemented yet.

--- Which Stream/Which Stream/AppDelegate.swift
+++ Which Stream/Which Stream/AppDelegate.swift
@@ -11,14 +11,52 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    let CLIENT_ID = "7b70f5162055cef2014f99d2538f3c1113ff484577d99de7508852d97c3dfa16"
+    let CLIENT_SECRET = "2f6955804d65303dbad29dc27e960b8979a660525748dd5d3fe699178159d6fc"
+    let USER_DEFAULTS = UserDefaults.standard
+    
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        // Retrieve the components attached to the url returned from call
+        let components = NSURLComponents(url: url, resolvingAgainstBaseURL: false)
+        // Convert names and values in query to a list of type NSURLQueryItem
+        let redirectData = (components?.queryItems)! as [NSURLQueryItem]
+        // Check which redirect URI is responsible for bringing the data back to the app
+        if (url.scheme == "which-simkl-authentication") {
+            if let key = redirectData.first?.name, let code = redirectData.first?.value {
+                if (key == "code") {
+                    let token_url = URL(string: "https://api.simkl.com/oauth/token")
+                    var req = URLRequest(url: token_url!)
+                    req.httpMethod = "POST"
+                    req.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                    req.httpBody = "{\n \"code\": \"\(code)\",\n \"client_id\": \"\(CLIENT_ID)\",\n \"client_secret\": \"\(CLIENT_SECRET)\",\n \"redirect_uri\": \"which-simkl-authentication://\",\n \"grant_type\": \"authorization_code\"\n}".data(using: .utf8)
+                    
+                    let task = URLSession.shared.dataTask(with: req) { data, response, error in
+                        do {
+                            if let data = data {
+                                let json = try JSONSerialization.jsonObject(with: data) as! [String: Any]
+                                let access_token = json["access_token"] as! String
+                                self.USER_DEFAULTS.set(access_token, forKey: "simkl-token")
+                            } else {
+                                debugPrint(error!)
+                            }
+                        } catch {
+                            debugPrint("Error deserializing JSON: \(error)")
+                        }
+                    }
+                    task.resume()
+                }
+            }
+        }
+        return true
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- Which Stream/Which Stream/ViewController.swift
+++ Which Stream/Which Stream/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
-
+import WebKit
 class ViewController: UIViewController {
-
+    // Simkl client id used for http request
+    let CLIENT_ID = "7b70f5162055cef2014f99d2538f3c1113ff484577d99de7508852d97c3dfa16"
+    // Redirect URI to return to application after http request
+    let REDIRECT_URI = "which-simkl-authentication://"
+    // Standard user data stored
+    let USER_DEFAULTS = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Retrieve code to make call for Simkl access token if necessary
+        if (USER_DEFAULTS.object(forKey: "simkl-token") == nil) {
+            generateToken()
+        }
+    }
+    
+    // Responsible for making a GET request to retrieve code necessary to retrieve Simkl's access token.
+    func generateToken() {
+        let url = URL(string: "https://simkl.com/oauth/authorize?response_type=code&client_id=\(self.CLIENT_ID)&redirect_uri=\(self.REDIRECT_URI)")!
+        UIApplication.shared.open(url, options: [:])
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
