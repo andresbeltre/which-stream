371e138f2122859ecef2b21fe8cdb8601c2e6c67
Leo Oliveira <macewandu@hotmail.com>
2018-08-26 23:17:01 -0400

    Finished RegisterViewController layout

--- Which Stream/Which Stream/LoginViewController.swift
+++ Which Stream/Which Stream/LoginViewController.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
+extension UITextField {
+
+    func setBottomBorder() {
+        self.borderStyle = .none
+        self.backgroundColor = .clear
+        
+        let borderLine = UIView(frame: CGRect(x: 0, y: self.frame.height - 1, width: self.frame.width * 3, height: 1))
+        borderLine.backgroundColor = .white
+        self.layer.masksToBounds = true
+        self.addSubview(borderLine)
+    }
+}
 class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
@@ -35,7 +47,7 @@ class LoginViewController: UIViewController {
         // Programatically create buttons, text fields, and labels
         let loginButton = UIButton(frame: CGRect(x: 0, y: 0, width: 60, height: 20))
         loginButton.titleLabel?.textColor = .white
-        loginButton.addTarget(self, action: #selector(normalAuthentication), for: .touchUpInside)
+        loginButton.addTarget(self, action: #selector(firebaseAuthentication), for: .touchUpInside)
         let cancelButton = UIButton(frame: CGRect(x: 0, y: 0, width: 60, height: 20))
         cancelButton.titleLabel?.textColor = .white
         cancelButton.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
@@ -44,8 +56,25 @@ class LoginViewController: UIViewController {
         facebookButton.addTarget(self, action: #selector(facebookAuthentication), for: .touchUpInside)
         let forgotPassButton = UIButton(frame: CGRect(x: 0, y: 0, width: 60, height: 20))
         forgotPassButton.titleLabel?.textColor = .white
+        forgotPassButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
         forgotPassButton.addTarget(self, action: #selector(forgotPassSegue), for: .touchUpInside)
         
+        let emailLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 80, height: 20))
+        emailLabel.textColor = .white
+        let passLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 80, height: 20))
+        passLabel.textColor = .white
+        let emailInput = UITextField(frame: CGRect(x: 0, y: 0, width: 120, height: 20))
+        emailInput.textColor = .white
+        emailInput.setBottomBorder()
+        let passInput = UITextField(frame: CGRect(x: 0, y: 0, width: 120, height: 20))
+        passInput.textColor = .white
+        passInput.isSecureTextEntry = true
+        passInput.setBottomBorder()
+        
+        // Assign correct text to respective labels
+        emailLabel.text = "Email:"
+        passLabel.text = "Password:"
+            
         // Assign correct text to respective buttons
         loginButton.setTitle("Login", for: .normal)
         cancelButton.setTitle("Cancel", for: .normal)
@@ -57,7 +86,11 @@ class LoginViewController: UIViewController {
         self.view.addSubview(cancelButton)
         self.view.addSubview(facebookButton)
         self.view.addSubview(forgotPassButton)
-        forgotPassButton.isHidden = true
+        self.view.addSubview(emailLabel)
+        self.view.addSubview(passLabel)
+        self.view.addSubview(emailInput)
+        self.view.addSubview(passInput)
+//        forgotPassButton.isHidden = true
         
         // Set translatesAutoresizingMaskIntoConstraints to false
         // since we're programatically creating the constraints
@@ -65,19 +98,35 @@ class LoginViewController: UIViewController {
         cancelButton.translatesAutoresizingMaskIntoConstraints = false
         facebookButton.translatesAutoresizingMaskIntoConstraints = false
         forgotPassButton.translatesAutoresizingMaskIntoConstraints = false
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        passLabel.translatesAutoresizingMaskIntoConstraints = false
+        emailInput.translatesAutoresizingMaskIntoConstraints = false
+        passInput.translatesAutoresizingMaskIntoConstraints = false
         
         // Constraints for login button
         loginButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 60).isActive = true
-        loginButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 90).isActive = true
+        loginButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 110).isActive = true
         // Constraints for cancel button
         cancelButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -55).isActive = true
-        cancelButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 90).isActive = true
+        cancelButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 110).isActive = true
         // Constraints for facebook button
         facebookButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         facebookButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 20).isActive = true
+        emailLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 80).isActive = true
+        emailLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -60).isActive = true
+        emailInput.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 80).isActive = true
+        emailInput.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -80).isActive = true
+        emailInput.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 10).isActive = true
+        passLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 80).isActive = true
+        passLabel.topAnchor.constraint(equalTo: emailInput.bottomAnchor, constant: 5).isActive = true
+        passInput.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 80).isActive = true
+        passInput.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -80).isActive = true
+        passInput.topAnchor.constraint(equalTo: passLabel.bottomAnchor, constant: 10).isActive = true
+        forgotPassButton.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -80).isActive = true
+        forgotPassButton.topAnchor.constraint(equalTo: passInput.bottomAnchor, constant: 5).isActive = true
     }
     
-    @objc func normalAuthentication() {
+    @objc func firebaseAuthentication() {
         
     }
     
--- Which Stream/Which Stream/RegisterViewController.swift
+++ Which Stream/Which Stream/RegisterViewController.swift
@@ -13,5 +13,114 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupBackgroundGradient()
+        setupLayout()
     }
+    
+    
+    func setupLayout() {
+        // Hide navigation bar
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        // Programatically create buttons, text fields, and labels
+        let registerButton = UIButton(frame: CGRect(x: 0, y: 0, width: 60, height: 20))
+        registerButton.titleLabel?.textColor = .white
+        registerButton.addTarget(self, action: #selector(sendEmail), for: .touchUpInside)
+
+        
+        let usernameLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 80, height: 20))
+        usernameLabel.textColor = .white
+        let emailLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 80, height: 20))
+        emailLabel.textColor = .white
+        let passwordLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 120, height: 20))
+        passwordLabel.textColor = .white
+        let retypePasswordLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 120, height: 20))
+        retypePasswordLabel.textColor = .white
+        let usernameInput = UITextField(frame: CGRect(x: 0, y: 0, width: 120, height: 20))
+        let emailInput = UITextField(frame: CGRect(x: 0, y: 0, width: 120, height: 20))
+        let passInput = UITextField(frame: CGRect(x: 0, y: 0, width: 120, height: 20))
+        let retypePassInput = UITextField(frame: CGRect(x: 0, y: 0, width: 120, height: 20))
+        
+        // Assign correct text to respective labels
+        usernameLabel.text = "Username:"
+        emailLabel.text = "Email:"
+        passwordLabel.text = "Password:"
+        retypePasswordLabel.text = "Retype Password:"
+        
+        // Assign correct text to respective buttons
+        registerButton.setTitle("Register", for: .normal)
+        
+        // Add buttons to view
+        self.view.addSubview(registerButton)
+        self.view.addSubview(usernameLabel)
+        self.view.addSubview(emailLabel)
+        self.view.addSubview(passwordLabel)
+        self.view.addSubview(retypePasswordLabel)
+        self.view.addSubview(usernameInput)
+        self.view.addSubview(emailInput)
+        self.view.addSubview(passInput)
+        self.view.addSubview(retypePassInput)
+ 
+        // Set translatesAutoresizingMaskIntoConstraints to false
+        // since we're programatically creating the constraints
+        registerButton.translatesAutoresizingMaskIntoConstraints = false
+        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        passwordLabel.translatesAutoresizingMaskIntoConstraints = false
+        retypePasswordLabel.translatesAutoresizingMaskIntoConstraints = false
+        usernameInput.translatesAutoresizingMaskIntoConstraints = false
+        emailInput.translatesAutoresizingMaskIntoConstraints = false
+        passInput.translatesAutoresizingMaskIntoConstraints = false
+        retypePassInput.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        usernameLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 80).isActive = true
+        usernameLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -80).isActive = true
+        usernameLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -60).isActive = true
+        usernameInput.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 80).isActive = true
+        usernameInput.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -80).isActive = true
+        usernameInput.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: 10).isActive = true
+        usernameInput.setBottomBorder()
+        emailLabel.topAnchor.constraint(equalTo: usernameInput.bottomAnchor, constant: 5).isActive = true
+        emailLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 80).isActive = true
+        emailLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -80).isActive = true
+        emailInput.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 10).isActive = true
+        emailInput.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 80).isActive = true
+        emailInput.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -80).isActive = true
+        emailInput.setBottomBorder()
+        passwordLabel.topAnchor.constraint(equalTo: emailInput.bottomAnchor, constant: 5).isActive = true
+        passwordLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 80).isActive = true
+        passwordLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -80).isActive = true
+        passInput.topAnchor.constraint(equalTo: passwordLabel.bottomAnchor, constant: 10).isActive = true
+        passInput.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 80).isActive = true
+        passInput.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -80).isActive = true
+        passInput.setBottomBorder()
+        retypePasswordLabel.topAnchor.constraint(equalTo: passInput.bottomAnchor, constant: 5).isActive = true
+        retypePasswordLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 80).isActive = true
+        retypePasswordLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -80).isActive = true
+        retypePassInput.topAnchor.constraint(equalTo: retypePasswordLabel.bottomAnchor, constant: 10).isActive = true
+        retypePassInput.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 80).isActive = true
+        retypePassInput.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -80).isActive = true
+        retypePassInput.setBottomBorder()
+        // Constraints for register button
+        registerButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        registerButton.topAnchor.constraint(equalTo: retypePassInput.bottomAnchor, constant: 20).isActive = true
+        
+    }
+    
+    @objc func sendEmail() {
+        
+    }
+    
+    func setupBackgroundGradient() {
+        self.view.backgroundColor = .clear
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        
+        gradientLayer.colors = [UIColor(hex: "5d0028").cgColor, UIColor(hex: "c96548").cgColor]
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
 }
